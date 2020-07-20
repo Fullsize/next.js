5017cf4428cb32072de2308a4307fd88f1832f73
无涯 <1619686166@qq.com>
2020-07-20 17:55:04 +0800

    feat: test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { Button, DatePicker } from 'antd';
 import './index.css';
 import 'antd/dist/antd.css';
-function Header(props:any) {
-	return(
-		<div styleName="box">
-			<Button type="primary">PRESS ME</Button>
-			header
-		</div>
-	)
+function Header(props: any) {
+  return (
+    <div styleName="box">
+      <Button type="primary">PRESS ME</Button>
+      header
+    </div>
+  );
 }
 export default Header;
