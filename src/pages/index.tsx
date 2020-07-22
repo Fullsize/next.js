126736ba5bf1ee759bfd8d40783eeef455c12b6a
无涯 <1619686166@qq.com>
2020-07-22 17:42:12 +0800

    feat: add jest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,36 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import dynamic from 'next/dynamic';
-import a from '@svg/smiling.svg';
-import { inject,observer } from 'mobx-react';
+import { inject } from 'mobx-react';
 import Swiper from '@/container/swiper';
-import './index.css'
+import './index.css';
 
 const Header = dynamic(() => import('@/components/Header'));
 function HomePage(props: any) {
-	const params = {
-		autoplay: true
-	}
-	useEffect(() => {
-		console.log(props)
-	}, [])
-	return (
-		<div>
-			<Header />
-			<div styleName="banner">
-				<Swiper {...params}>
-					<div styleName="banner-item">
-						<img src="/img/1.jpg" alt="" />
-					</div>
-					<div styleName="banner-item">
-						<img src="/img/2.jpg" alt="" />
-					</div>
-					<div styleName="banner-item">
-						<img src="/img/3.jpg" alt="" />
-					</div>
-				</Swiper>
-
-			</div>
-
-		</div>
-	)
+  const params = {
+    autoplay: true,
+  };
+  useEffect(() => {
+    console.log(props);
+  }, []);
+  return (
+    <div>
+      <Header />
+      <div styleName="banner">
+        <Swiper {...params}>
+          <div styleName="banner-item">
+            <img src="/img/1.jpg" alt="" />
+          </div>
+          <div styleName="banner-item">
+            <img src="/img/2.jpg" alt="" />
+          </div>
+          <div styleName="banner-item">
+            <img src="/img/3.jpg" alt="" />
+          </div>
+        </Swiper>
+      </div>
+    </div>
+  );
 }
 export default inject((stores: any) => ({
-	name: stores.userStore.name
+  name: stores.userStore.name,
 }))(HomePage);
